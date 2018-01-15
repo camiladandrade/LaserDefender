78ca4a304ba6c878575bf92184fb13ba881c4a9a
Camila Andrade <camilamdandrade@gmail.com>
2018-01-15 17:59:55 -0300

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	public float paddig = 0.5f;
+	
+	//float speedTime = speed * Time.deltaTime;
+	float xMin;
+	float xMax;
 
 	// Use this for initialization
 	void Start () {
-	
+		SpaceSize();
 	}
 	
 	// Update is called once per frame
@@ -17,14 +22,27 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void SpaceMovement(){
-		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
-		} else if (Input.GetKey(KeyCode.DownArrow)){
-			transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
-		} else if (Input.GetKey(KeyCode.RightArrow)){
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}else if(Input.GetKey(KeyCode.D)){
 			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
 		} else if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		}else if (Input.GetKey(KeyCode.A)){
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
 		}
+		
+		//restrict the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+	
+	void SpaceSize(){
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		
+		xMin = leftMost.x + paddig;
+		xMax = rightMost.x - paddig;
 	}
 }
